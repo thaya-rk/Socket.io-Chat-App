eb1ad9cf0720852117632ace4bc133945fdd4f9b
thayark <rkt12357@gmail.com>
2024-04-04 23:28:07 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import {createServer} from "http"
+import {Server} from "socket.io"
+
+const httpServer=createServer()
+
+const io =new Server(httpServer,{
+    cors:process.env.NODE_ENV === "production" ? false : ["http://localhost:5500","http://127.0.0.1:localhost:5500"]
+})
+
+io.on('connection',socket=>{
+    console.log(`user ${socket.id} connected`)
+
+    socket.on('message',data=>{
+        console.log(data)
+        io.emit('message', `${socket.id.substring(0,5)}: ${data}`)
+    })
+})
+
+httpServer.listen(3500,()=> console.log(`listening on port 3500`))
